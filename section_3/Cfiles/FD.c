7b224da2756fe36676efeead51ee13efe4d3d169
oXo2018 <xuyingyang2017@gmail.com>
2023-12-13 01:37:35 +0800

    mofify FD.c to add seting file status descriptor!

--- section_3/Cfiles/FD.c
+++ section_3/Cfiles/FD.c
@@ -3,6 +3,45 @@
 #include "unistd.h"
 #include <fcntl.h>
 
+int  set_fl(int fd, int flags) // flags are file status flags to turn on
+{
+    int val;
+    if ( (val = fcntl(fd, F_GETFL , 0)) < 0)
+    {
+        printf("fcntl F_GETFL error");
+        return -1;
+    }
+
+    val |= flags; // turn on flags
+
+    if (fcntl(fd, F_SETFL, val) < 0)
+    {
+        printf("fcntl F_SETFL error");
+        return -1;
+    }
+    return 0;
+}
+
+int  clr_fl(int fd, int flags) // flags are file status flags to turn off
+{
+    int val;
+    if ( (val = fcntl(fd, F_GETFL , 0)) < 0)
+    {
+        printf("fcntl F_GETFL error");
+        return -1;
+    }
+
+    val &= ~flags; // turn off flags
+
+    if (fcntl(fd, F_SETFL, val) < 0)
+    {
+        printf("fcntl F_SETFL error");
+        return -1;
+    }
+    return 0;
+}
+
+
 int main(int argc, char *argv[])
 {
     int val;
@@ -11,11 +50,13 @@ int main(int argc, char *argv[])
         return -1;
     }
 
+    // when using the `F_GETFL` operation, the third argument is typically ignored or set to 0.
     if ( (val = fcntl(atoi(argv[1]), F_GETFL, 0)) < 0){
         printf("fcntl error for fd %d", atoi(argv[1]));
         return -1;
     }
 
+    // By utilizing bitwise operations and a mask, `val & O_ACCMODE` preserves the portion of files status flags related to access mode, while zeroing out other bits. 
     switch (val & O_ACCMODE)
     {
     case O_RDONLY:
